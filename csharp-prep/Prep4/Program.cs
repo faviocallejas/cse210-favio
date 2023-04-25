f02b1b4350a21d5b2ead242e7479cc6a2e61c5eb
Favio <favioc810@gmail.com>
2023-04-25 18:59:47 -0400

    Completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);            
+            }
+        }
+        Console.WriteLine(" ");
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        int min = 9999;
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < min)
+            {
+                min = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+        Console.WriteLine($"The smallest positive number is: {min}");
+        Console.WriteLine("");
+        Console.WriteLine("The sorte list is:");
+
+        int[] sorting = numbers.ToArray();
+        Array.Sort(sorting);
+        foreach (int value in sorting)
+        {
+            Console.Write(value);
+            Console.Write(' ');
+        }
+        Console.ReadLine();
     }
 }
