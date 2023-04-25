21096708dbdd2701ca355289a8e0f462172859d0
Favio <favioc810@gmail.com>
2023-04-24 23:37:14 -0400

    First attempt, review

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = "yes";
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            int number = 0;
+            int attempts  = 0;
+            //for (int attempts = 0; attempts < 100; attempts++)
+            while (attempts < 100)
+            {
+                while (number != magicNumber)
+                {
+                    Console.Write("What is your guess?: ");
+                    number = int.Parse(Console.ReadLine());
+                    
+                    if (magicNumber > number)
+                    {
+                        Console.WriteLine("Higher");
+                        attempts++;
+                    }
+                    else if (magicNumber < number)
+                    {
+                        Console.WriteLine("Lower");
+                        attempts++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                        Console.WriteLine($"You did it in {attempts+1} attempts"); 
+                    }
+                }
+            }   Console.Write("Do you want to play again?, press 0 to exit the program, any other number to play again: ");
+                response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
