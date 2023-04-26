dc39972cb1c603ad9c2e374b260d74a8de31cfa1
Favio <favioc810@gmail.com>
2023-04-26 17:20:39 -0400

    Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,40 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        string response = "yes";
-        do
-        {
-            Random randomGenerator = new Random();
-            int magicNumber = randomGenerator.Next(1, 100);
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
 
-            int number = 0;
-            int attempts  = 0;
+        int number = 0;
+        int attempts  = 0;
             //for (int attempts = 0; attempts < 100; attempts++)
-            while (attempts < 100)
+        while (attempts < 100)
+        {
+            while (number != magicNumber)
             {
-                while (number != magicNumber)
+                Console.Write("What is your guess?: ");
+                number = int.Parse(Console.ReadLine());
+                
+                if (magicNumber > number)
                 {
-                    Console.Write("What is your guess?: ");
-                    number = int.Parse(Console.ReadLine());
-                    
-                    if (magicNumber > number)
-                    {
-                        Console.WriteLine("Higher");
-                        attempts++;
-                    }
-                    else if (magicNumber < number)
-                    {
-                        Console.WriteLine("Lower");
-                        attempts++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("You guessed it!");
-                        Console.WriteLine($"You did it in {attempts+1} attempts"); 
-                    }
+                    Console.WriteLine("Higher");
+                    attempts++;
                 }
-            }   Console.Write("Do you want to play again?, press 0 to exit the program, any other number to play again: ");
-                response = Console.ReadLine();
-        } while (response == "yes");
-    }
+                else if (magicNumber < number)
+                {
+                    Console.WriteLine("Lower");
+                    attempts++;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You did it in {attempts+1} attempts");
+                    break; 
+                }
+            }            
+        }
+    }    
 }
