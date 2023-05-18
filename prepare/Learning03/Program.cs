78b8523fb52f7c8d0e2f2ddd19bd52c236ece1c9
Favio <favioc810@gmail.com>
2023-05-18 00:55:42 -0400

    Assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction a = new Fraction();
+        Console.WriteLine(a.GetFractionString());
+        Console.WriteLine(a.GetDecimalValue());
+
+        Fraction b = new Fraction(6);
+        Console.WriteLine(b.GetFractionString());
+        Console.WriteLine(b.GetDecimalValue());
+
+        Fraction c = new Fraction(6, 7);
+        Console.WriteLine(c.GetFractionString());
+        Console.WriteLine(c.GetDecimalValue());
     }
 }
